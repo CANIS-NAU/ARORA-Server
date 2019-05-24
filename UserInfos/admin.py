5c48c847e5aeffc9076d1f48c68c55950d32fab6
LooDaHu <40339075+LooDaHu@users.noreply.github.com>
2019-05-24 11:35:45 -0700

    Update admin.py
    replace the Django default UserAdmin with UserInfoAdmin, then UserInfo is accessible at admin-site.

--- UserInfos/admin.py
+++ UserInfos/admin.py
@@ -1,7 +1,16 @@
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 from .models import UserInfo
 
-admin.site.register(UserInfo, UserAdmin)
+
+# Create UserInfoAdmin for UserInfo model.
+class UserInfoAdmin(admin.ModelAdmin):
+	#  Set attributes to be displayed
+        list_display = ['username','user_id','user_points'] 
+	# For more information, https://docs.djangoproject.com/en/2.2/ref/contrib/admin/
+
+# let our admin-site know the new member coming
+# Two arguments here, one is the model, the other is related admin class
+admin.site.register(UserInfo, UserInfoAdmin)
 
