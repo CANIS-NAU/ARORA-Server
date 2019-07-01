2f50dca257e243c3c16c53d20eead6a37ca14ddd
Morgan Vigil-Hayes <morgan.vigil-hayes@nau.edu>
2019-07-01 17:23:54 +0000

    Re-adding user_name

--- UserInfos/models.py
+++ UserInfos/models.py
@@ -10,7 +10,7 @@ class UserInfo(AbstractUser):
     user_current_mood_updated = models.DateTimeField(default=datetime(1979, 12, 31, 0, 0, 0, 000000, tzinfo=utc),
                                                      db_column='UserCurrentMoodUpdated')
     user_created_at = models.DateTimeField(auto_now_add=True, editable=False, db_column='UserCreatedAt')
-    #user_name = models.TextField(default='', db_column='UserName')
+    user_name = models.TextField(default='', db_column='UserName')
     user_current_mood = models.IntegerField(default=0, db_column='UserCurrentMood')
     user_id = models.IntegerField(default=2147483648, unique=False, editable=False, db_column='UserId')
     user_current_location_lat = models.DecimalField(max_digits=5, decimal_places=2, default=0.0,
